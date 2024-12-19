a84d2e6603fae63f2157a8ef59e307a9247843d0
zh1995 <rwzsjfm@qq.com>
2024-12-19 21:02:13 +0800

    get params example

--- main.go
+++ main.go
@@ -21,6 +21,22 @@ func main() {
 		c.String(http.StatusOK, "This is a POST request")
 	})
 
+	r.GET("/user/:name/*action", func(c *gin.Context) {
+		name := c.Param("name")
+		action := c.Param("action")
+		message := name + " is " + action
+		c.String(http.StatusOK, message)
+	})
+
+	r.POST("/user/:name/*action", func(c *gin.Context) {
+		b := c.FullPath() == "/user/:name/*action"
+		c.String(http.StatusOK, "%t", b)
+	})
+
+	r.GET("/user/groups", func(c *gin.Context) {
+		c.String(http.StatusOK, "The avaliable groups are [...]")
+	})
+
 	// 启动HTTP服务器，默认监听在0.0.0.0:8080
 	r.Run()
 }
