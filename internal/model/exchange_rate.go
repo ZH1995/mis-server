9d67a8481df5204395db10faf38a20f8e038038c
zh1995 <rwzsjfm@qq.com>
2025-01-09 19:39:51 +0800

    init project

--- internal/model/exchange_rate.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type ExchangeRate struct {
-	ID           uint    `json:"id" gorm:"primarykey"`
-	FromCurrency string  `json:"from_currency" binding:"required"`
-	ToCurrency   string  `json:"to_currency" binding:"required"`
-	Rate         float64 `json:"rate" binding:"required"`
-	Date         int64   `json:"date"`
-}
